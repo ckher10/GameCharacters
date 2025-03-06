fa86240d1280c2d33f9c202a6d7706622fdd871d
Cheng Her <ckher96@gmail.com>
2025-03-06 13:59:12 -0600

    [1.09] Create user menu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using NLog;
 using System.Text.Json;
-
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -11,5 +10,35 @@ logger.Info("Program started");
 // deserialize mario json from file into List<Mario>
 string marioFileName = "mario.json";
 List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
+
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Display Mario Characters");
+  Console.WriteLine("2) Add Mario Character");
+  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("Enter to quit");
+
+  // input selection
+  string? choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+
+  if (choice == "1")
+  {
+    // Display Mario Characters
+  }
+  else if (choice == "2")
+  {
+    // Add Mario Character
+  }
+  else if (choice == "3")
+  {
+    // Remove Mario Character
+  } else if (string.IsNullOrEmpty(choice)) {
+    break;
+  } else {
+    logger.Info("Invalid choice");
+  }
+} while (true);
 
 logger.Info("Program ended");
